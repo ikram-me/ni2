65cd46a824bf1d250bb4d6ed72ffe76cb24e76aa
ikram-m <62060750+ikram-m@users.noreply.github.com>
2022-03-19 23:05:58 +0300

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import moment from "moment";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
+import DateRangePicker from "rnv-date-range-picker";
 
+
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View >
+  <Text>Bezier Line Chart</Text>
+  <SafeAreaView>
+      <View style={styles.container}>
+        <DateRangePicker
+          onSelectDateRange={(range) => {
+            setRange(range);
+          }}
+          responseFormat="YYYY-MM-DD"
+          maxDate={moment()}
+          minDate={moment().subtract(100, "days")}
+        />
+        <View style={styles.container}>
+          <Text>first date: {selectedRange.firstDate}</Text>
+          <Text>second date: {selectedRange.secondDate}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel={"$"}
+    yAxisSuffix={"k"}
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{chartConfig,
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
